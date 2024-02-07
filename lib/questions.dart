01fdf58e0abbc4b53e7f544bd51aa2ca5b26233e
kraianan Rattana <longhai044207@gmail.com>
2024-02-07 15:52:54 +0700

    first commit

---
+++ lib/questions.dart
@@ -0,0 +1,104 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question : '1. Who is the founder of facebook?',
+    correctAnswerIndex : 1,
+    options: [
+      'a) Steve Jobs',
+      'b) Mark Zuckerberg',
+      'c) Bill Gates',
+      'd) Elon Musk',
+    ],
+  ),
+  Question(
+    question : '2. What is the NEWEST iPhone?',
+    correctAnswerIndex : 1,
+    options: [
+      'a) iPhone 14',
+      'b) iPhone 15',
+      'c) iPhone 16',
+      'd) iPhone 17',
+    ],
+  ),
+  Question(
+    question : '3. What name of X before?',
+    correctAnswerIndex : 2,
+    options: [
+      'a) Twich',
+      'b) Twerk',
+      'c) Twitter',
+      'd) Twingle twingle little star',
+    ],
+  ),
+  Question(
+    question : '4. What is Youtube?',
+    correctAnswerIndex : 3,
+    options: [
+      'a) Social media',
+      'b) News',
+      'c) Video games',
+      'd) Video and Music',
+    ],
+  ),
+  Question(
+    question : '5. What is Razer logo look like?',
+    correctAnswerIndex : 1,
+    options: [
+      'a) Snack',
+      'b) Snake',
+      'c) Chemicals',
+      'd) Danger sign',
+    ],
+  ),
+  Question(
+    question : '6. What is answer of 1+1 in binary number?',
+    correctAnswerIndex : 2,
+    options: [
+      'a) 2',
+      'b) 1',
+      'c) 10',
+      'd) 11',
+    ],
+  ),
+  Question(
+    question : '7. What is mean of VS code?',
+    correctAnswerIndex : 3,
+    options: [
+      'a) Vicual Studio code',
+      'b) Vasco code',
+      'c) Visal code',
+      'd) Visual Studio code',
+    ],
+  ),
+  Question(
+    question : '8. What is answer of 20 % 4?',
+    correctAnswerIndex : 0,
+    options: [
+      'a) 0',
+      'b) 5',
+      'c) 20',
+      'd) 4',
+    ],
+  ),
+  Question(
+    question : '9. What is top 1 browser app 2024?',
+    correctAnswerIndex : 0,
+    options: [
+      'a) Firefox',
+      'b) Google Chrome',
+      'c) Microsoft Edge',
+      'd) Apple Safari',
+    ],
+  ),
+  Question(
+    question : '10. What grade I should get?',
+    correctAnswerIndex : 0,
+    options: [
+      'a) A',
+      'b) B',
+      'c) C',
+      'd) D',
+    ],
+  ),
+];
